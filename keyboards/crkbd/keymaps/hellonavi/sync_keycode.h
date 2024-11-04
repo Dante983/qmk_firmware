1a698b02146699797ef2125c6ac6648083d11145
Nikola Savic <nikola@performcb.com>
2024-11-04 13:41:37 +0100

    added hellonavi to crkbd

---
+++ keyboards/crkbd/keymaps/hellonavi/sync_keycode.h
@@ -0,0 +1,11 @@
+// sync_keycode.h
+#ifndef SYNC_KEYCODE_H
+#define SYNC_KEYCODE_H
+
+#include <stdint.h> // Include this header for uint16_t
+
+typedef struct {
+    uint16_t keycode;
+} sync_keycode_t;
+
+#endif // SYNC_KEYCODE_H
